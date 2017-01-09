7d2706e52a4d903cf0054689feba0bb92e85c2e7
KeyboardFire <andy@keyboardfire.com>
2017-01-08 19:24:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## ppi3
+
+**ppi3** is a preprocessor for the i3 window manager.
+
+Sample usage:
+
+    ./ppi3.rb <~/.i3/_config >~/.i3/config
---
+++ ppi3.rb
@@ -0,0 +1,121 @@
+#!/usr/bin/env ruby
+
+# ppi3 - preprocessor for the i3 window manager
+# Copyright (C) 2017  Keyboard Fire <andy@keyboardfire.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+def expand line, expansions, groups
+    return line if expansions.empty?
+
+    lines = ''
+    pos = Hash.new 0
+
+    loop {
+
+        newline = line.clone
+        expansions.reverse.each do |exp|
+            newline.insert exp[:pos], exp[:values][pos[exp[:group]] % exp[:values].length]
+        end
+        lines += newline + ?\n
+
+        idx = 0
+        while (pos[groups.keys[idx]] += 1) == groups[groups.keys[idx]]
+            pos[groups.keys[idx]] = 0
+            idx += 1
+            return lines.chomp if idx == groups.keys.length
+        end
+
+    }
+end
+
+def preprocess config
+    global = binding
+
+    # kill comments
+    config.gsub! /#+( .*)?\n/, ?\n
+
+    # also annoying leading/trailing/repeated whitespace
+    config.gsub! /^\s+|[ \t]+$/, ''
+    config.gsub! /[ \t]+/, ' '
+
+    # line continuations
+    config.gsub! /\\\n\s*/, ''
+
+    # go through the string matching braces
+    nest_positions = []
+    pos = 0
+    while match = config.match(/([{}])/, pos)
+        pos = match.begin(0) + 1
+        case match[0]
+        when ?{
+            nest_positions.push pos
+        when ?}
+            nest_pos = nest_positions.pop
+            if m = config[0...nest_pos].match(/^(group|define) "([^"]+)" {\z/)
+                contents = config[m.end(0)...match.begin(0)].strip
+                pos = m.begin(0)
+                case m[1]
+                when 'group'
+                    eval "$#{m[2]} = #{contents.inspect}"
+                    config[m.begin(0)..match.begin(0)] = contents
+                when 'define'
+                    eval "$#{m[2]} = #{contents}"
+                    config[m.begin(0)..match.begin(0)] = ''
+                end
+            elsif config[nest_pos-1..nest_pos] == '{{'
+                config[nest_pos-1..match.begin(0)] =
+                    eval(config[nest_pos+1...match.begin(0)-1]).to_s
+                pos = nest_pos
+            end
+        end
+    end
+
+    # now we go through linewise for the *sh-like brace expansions
+    config.gsub!(/.+/) {|line|
+        expansions = []
+        groups = Hash.new 0
+        nil while line.sub!(/\{[^}]*\}/) do
+            match = $~
+            group, values = match[0][1...-1].split ?|, 2
+            if values.nil?
+                values = group
+                group = '__default_group__'
+            end
+            values = values.split(?,, -1).flat_map{|val|
+                val =~ /^(.+)\.\.(.+)$/ ? [*$1..$2] : val
+            }
+            expansions.push({
+                group: group,
+                values: values,
+                pos: match.begin(0)
+            })
+            groups[group] = [groups[group], values.length].max
+            ''
+        end
+
+        expand line, expansions, groups
+    }
+
+    # # cleanup: remove empty lines
+    # config.gsub! /\n+/, "\n"
+    # config.sub! /\A\n/, ''
+
+    config
+
+end
+
+if __FILE__ == $0
+    puts preprocess STDIN.read
+end
